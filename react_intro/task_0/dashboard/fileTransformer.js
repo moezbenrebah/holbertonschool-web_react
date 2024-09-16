046888dcaefedf4ed16b078a0070ccfff0ad0936
moezbenrebah <benrebah.moez@gmail.com>
2024-09-16 10:55:25 +0100

    update test file import path

--- react_intro/task_0/dashboard/fileTransformer.js
+++ react_intro/task_0/dashboard/fileTransformer.js
@@ -2,7 +2,7 @@ import path from 'path';
 
 
 export default {
-    process(sourceText, sourcePath, options) {
+    process(sourceText, sourcePath) {
         return {
             code: `module.exports = ${JSON.stringify(path.basename(sourcePath))};`,
         };

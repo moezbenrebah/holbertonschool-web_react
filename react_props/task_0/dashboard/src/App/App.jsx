67971640e2d20ef0704b5c778495731ff1b377f8
moezbenrebah <benrebah.moez@gmail.com>
2024-09-19 14:06:57 +0100

    add task 0

---
+++ react_props/task_0/dashboard/src/App/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Notifications from '../Notifications/Notifications';
+import Header from '../Header/Header';
+import Login from '../Login/Login';
+import Footer from '../Footer/Footer';
+
+function App() {
+  return (
+    <>
+      <Notifications />
+      <Header />
+      <Login />
+      <Footer />
+    </>
+  );
+}
+
+export default App;

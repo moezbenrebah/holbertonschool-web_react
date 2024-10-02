20b5e0350a5cb12ef153491b10a136052e85d7c3
moezbenrebah <benrebah.moez@gmail.com>
2024-10-02 11:36:06 +0100

    fix the import statement

---
+++ react_component/task_2/dashboard/src/Notifications/Notifications.jsx
@@ -0,0 +1,64 @@
+import { Component } from 'react';
+import './Notifications.css';
+import closeIcon from '../assets/close-icon.png';
+import NotificationItem from './NotificationItem';
+
+class Notifications extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  markAsRead = (id) => {
+    console.log(`Notification ${id} has been marked as read`);
+  }
+
+  shouldComponentUpdate(nextProps) {
+    return (
+      nextProps.notifications.length >
+        this.props.notifications.length ||
+      nextProps.displayDrawer !== this.props.displayDrawer
+    );
+  }
+
+  render() {
+    const { notifications = [], displayDrawer = false } = this.props;
+    return (
+      <>
+        <div className="notification-title">Your notifications</div>
+        {
+          displayDrawer ? (
+            <div className='Notifications'>
+              {notifications.length > 0 ? (
+                <>
+                  <p>Here is the list of notifications</p>
+                  <button
+                    onClick={() => console.log('Close button has been clicked')}
+                    aria-label='Close'
+                  >
+                    <img src={closeIcon} alt='close icon' />
+                  </button>
+                  <ul>
+                    {notifications.map((notification, index) => (
+                      <NotificationItem
+                        key={index}
+                        type={notification.type}
+                        value={notification.value}
+                        html={notification.html}
+                        markAsRead={this.markAsRead}
+                      />
+                    ))}
+                  </ul>
+                </>
+              ) : (
+                <p>No new notification for now</p>
+              )}
+            </div>
+          ) :
+          ([])
+        }
+      </>
+    );
+  }
+}
+
+export default Notifications;

ee6ee5f2dcb52d4d6414ab63a23120da923d2e83
moezbenrebah <benrebah.moez@gmail.com>
2024-10-02 10:47:23 +0100

    improve unit tests

--- react_component/task_1/dashboard/src/App/App.jsx
+++ react_component/task_1/dashboard/src/App/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import { Component } from 'react';
 import './App.css';
 import Notifications from '../Notifications/Notifications';
 import Footer from '../Footer/Footer';

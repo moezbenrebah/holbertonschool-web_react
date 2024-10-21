b94a2f865e38627b6317fc06ecd049a303069cb4
moezbenrebah <benrebah.moez@gmail.com>
2024-10-21 14:34:23 +0100

    add updates

---
+++ react_state/task_4/dashboard/src/Footer/Footer.jsx
@@ -0,0 +1,25 @@
+import './Footer.css';
+import { getCurrentYear, getFooterCopy } from '../utils/utils';
+import { newContext } from '../Context/context';
+
+function Footer() {
+
+  return (
+    <newContext.Consumer>
+      {
+        (context) => {
+          if (context.user) {
+            return (
+              <div className="App-footer">
+                <p>Copyright {getCurrentYear()} - {getFooterCopy(true)}</p>
+                {context.user.isLoggedIn && <a href="#">Contact us</a>}
+              </div>
+            )
+          }
+        }
+      }
+    </newContext.Consumer>
+  )
+}
+
+export default Footer;

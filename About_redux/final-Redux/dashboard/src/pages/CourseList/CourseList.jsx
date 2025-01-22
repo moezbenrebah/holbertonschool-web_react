df45fd5620c6bec570c8e1e581a1c708a9ac92d7
moezbenrebah <benrebah.moez@gmail.com>
2025-01-22 17:34:28 +0100

    set up loading state

---
+++ About_redux/final-Redux/dashboard/src/pages/CourseList/CourseList.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import CourseListRow from './CourseListRow/CourseListeRow';
+import './CourseList.css';
+import WithLogging from '../../components/HOC/WithLogging';
+
+function CourseList() {
+  const { courses } = useSelector((state) => state.courses);
+
+  return (
+    <div className="courses">
+      {courses.length > 0 ? (
+        <table id="CourseList">
+          <thead>
+            <CourseListRow textFirstCell="Available courses" isHeader={true} />
+            <CourseListRow textFirstCell="Course name" textSecondCell="Credit" isHeader={true} />
+          </thead>
+          <tbody>
+            {courses.map((course) => (
+              <CourseListRow key={course.id} textFirstCell={course.name} textSecondCell={course.credit} />
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <table id="CourseList">
+          <thead>
+            <CourseListRow isHeader={true} textFirstCell="No course available yet" />
+          </thead>
+        </table>
+      )}
+    </div>
+  );
+}
+
+export default WithLogging(CourseList);
+

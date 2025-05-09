70d8daffba541f29fac684504d2c3b26d46169ae
moezbenrebah <boul3ez07.23@gmail.com>
2025-05-09 20:55:48 +0100

    fix notification panel in mobile screens

--- react_styling/task_4/dashboard/src/App/App.jsx
+++ react_styling/task_4/dashboard/src/App/App.jsx
@@ -46,9 +46,9 @@ export default class App extends Component {
     const { isLoggedIn = true, logOut = () => {} } = this.props;
 
     return (
-      <div className="relative px-3 w-[calc(100%-24px)] mx-auto min-h-screen flex flex-col justify-between max-[520px]:px-0 max-[912px]:overflow-y-hidden max-[912px]:h-screen max-[430px]:overflow-y-hidden [430px]:h-screen">
+      <div className="relative px-3 w-[calc(100%-24px)] mx-auto min-h-screen flex flex-col justify-between max-[520px]:px-0 max-[912px]:top-0 max-[912px]:overflow-y-auto [912px]:h-screen">
         <div>
-          <Notifications notifications={notificationsList} />
+          <Notifications notifications={notificationsList}/>
           <Header />
           <main>
             {
--- react_styling/task_4/dashboard/src/Notifications/Notifications.jsx
+++ react_styling/task_4/dashboard/src/Notifications/Notifications.jsx
@@ -19,14 +19,14 @@ export default class Notifications extends React.Component {
   }
 
   render() {
-    const { notifications = [], displayDrawer = true } = this.props;
+    const { notifications = [], displayDrawer = false } = this.props;
 
     return (
       <>
         <div className="notification-title absolute right-3 top-1">Your notifications</div>
         {
           displayDrawer ? (
-            <div className='Notifications relative border-[3px] border-dotted border-[color:var(--main-color)] p-1.5 w-1/4 float-right mt-7 max-[912px]:w-full max-[912px]:fixed max-[912px]:top-0 max-[912px]:left-0 max-[912px]:right-0 max-[912px]:bottom-0 max-[912px]:z-50 max-[912px]:float-none max-[912px]:m-0 max-[912px]:p-3 max-[912px]:bg-white max-[912px]:overflow-y-auto max-[912px]:h-screen'>
+            <div className='Notifications relative border-[3px] border-dotted border-[color:var(--main-color)] p-1.5 w-1/4 float-right mt-7 max-[912px]:w-full max-[912px]:fixed max-[912px]:top-0 max-[912px]:left-0 max-[912px]:right-0 max-[912px]:bottom-0 max-[912px]:z-50 max-[912px]:float-none max-[912px]:m-0 max-[912px]:p-3 max-[912px]:bg-white max-[912px]:overflow-y-hidden max-[912px]:h-screen max-[430px]:overflow-y-hidden max-[430px]:h-screen'>
               {notifications.length > 0 ? (
                 <div className='relative'>
                   <p className="m-0 max-[912px]:text-[20px]">Here is the list of notifications</p>
@@ -37,7 +37,7 @@ export default class Notifications extends React.Component {
                   >
                     <img src={closeIcon} alt='close icon' className="w-3 h-3" />
                   </button>
-                  <ul className='list-[square] pl-5 max-[912px]:p-0 max-[912px]:list-none'>
+                  <ul className='list-[square] pl-5 max-[912px]:p-0 max-[912px]:list-none max-[430px]:overflow-y-hidden max-[430px]:h-screen'>
                     {notifications.map((notification, index) => (
                       <NotificationItem
                         id={index}
